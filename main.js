4a59bb8c39fe3fe5ad775c952410fb49bfa8dcd2
faizkhan12 <faizk6797@gmail.com>
2022-10-02 23:37:45 +0530

    Add starter template

---
+++ main.js
@@ -0,0 +1,67 @@
+import * as THREE from "three";
+
+export default class ThreeD {
+  constructor() {
+    // init
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    // this.renderer.setAnimationLoop(animation);
+    document.getElementById("app").appendChild(this.renderer.domElement);
+
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.01,
+      10
+    );
+    this.camera.position.z = 1;
+
+    this.scene = new THREE.Scene();
+    this.addMesh();
+    this.time = 0;
+    this.render();
+    this.resize();
+  }
+
+  addMesh() {
+    this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+    this.material = new THREE.MeshNormalMaterial();
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
+  }
+  render() {
+    this.time++;
+    this.mesh.rotation.x = this.time / 2000;
+    this.mesh.rotation.y = this.time / 1000;
+
+    this.renderer.render(this.scene, this.camera);
+    window.requestAnimationFrame(this.render.bind(this.time));
+  }
+
+  resize() {
+    /**
+     * Sizes
+     */
+    const sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+
+    window.addEventListener("resize", () => {
+      // Update sizes
+      sizes.width = window.innerWidth;
+      sizes.height = window.innerHeight;
+
+      // Update camera
+      this.camera.aspect = sizes.width / sizes.height;
+      this.camera.updateProjectionMatrix();
+
+      // Update renderer
+      this.renderer.setSize(sizes.width, sizes.height);
+      this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    });
+  }
+}
+
+new ThreeD();
